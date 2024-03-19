8cc51f8c2de95326e0ad3d8a09cca23f0da1b1b7
Erlyn Naranjo Burgos <erlynstuars@gmail.com>
2024-03-18 23:47:48 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI, Body, HTTPException, Path, Query
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel, Field
+from typing import Optional
+
+app = FastAPI(title="Segundo Ejercicio")
+app.version = "0.0.1"
+
+class Movie(BaseModel):
+    id:Optional[int] = None
+    title : str = Field( min_length=5, max_length=15)
+    overview: str  = Field( min_length=15, max_length=55)
+    year: int =  Field( le=2024, max_length=15)
+    rating: float = Field(ge=1, le=10)
+    category: str = Field(min_length=5, max_length=15)
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "Mi pelicula",
+                "overview": "ASD123456",
+                "year": 2022,
+		"rating": 9.8,
+		"category": "Acción"
+
+            }
+        }
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Terror"
+    }
+]
+
+@app.get("/", tags=['Home']) 
+def root():
+    return HTMLResponse("<h1>Hola esto es uan prueba </h1>")
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+  
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int =  Path(ge=1, le=2000)):
+    movie = filter(lambda item: item["id"] == id, movies)
+    try: return list(movie)[0]
+    except: raise HTTPException(404,detail={"error":"Esto es algo" })
+
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)):
+    return [i for i in movies if i['category']== category ]
+
+@app.post("/movies",tags=['movies'])
+def create_movie(movie: Movie):
+    movies.append(movie)
+    return movies
+
+
+@app.put('/movies/{id}', tags=['movies'])
+def update_movie(id:int, movie:Movie):
+    for item in movies:
+        if item["id"] == id:
+            item['title'] = movie.title
+            item['overview'] = movie.overview
+            item['year'] = movie.year
+            item['rating'] = movie.rating
+            item['category'] = movie.category
+            return movies
+
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            movies.remove(item)
+            return movies
